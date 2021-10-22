c21a2b22b62e1b239755c06ea3898b39669badbb
LJ71 <lancejoseph@hotmail.com>
2021-10-21 23:09:32 -0400

    added jquery interaction and reconfigured footer

--- js/index.js
+++ js/index.js
@@ -2,3 +2,12 @@ $(".connect").on("click", function () {
   $(".modal").fadeIn(500);
   $(".overlay").fadeIn(250);
 });
+function sendEmail() {
+  $("#name, #email, #message").val("");
+  closeModal();
+  alert("Email has been submitted. I'll reach out to you shortly. Thank You!");
+}
+function closeModal() {
+  $(".modal").fadeOut(500);
+  $(".overlay").fadeOut(250);
+}
